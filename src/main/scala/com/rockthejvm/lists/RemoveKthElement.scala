f795cb951fb631cf1dbdcc8fef79218d7ab45e38
Nick <nikolayvaklinov@yahoo.com>
2020-04-04 13:32:27 +0100

    remove k-th element

---
+++ src/main/scala/com/rockthejvm/lists/RemoveKthElement.scala
@@ -0,0 +1,102 @@
+package com.rockthejvm.lists
+
+
+import scala.annotation.tailrec
+
+
+sealed abstract class RList[+T] {
+  def head: T
+  def tail: RList[T]
+  def isEmpty: Boolean
+
+  def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
+
+  // reverse the list
+  def reverse: RList[T]
+
+  // concatenate another list in this one
+  def ++[S >: T](anotherList: RList[S]): RList[S]
+
+  // remove an element at a given index, return a NEW list
+//  def reverseAt: RList[T]
+
+  def removeAt(index: Int): RList[T]
+}
+
+case object RNil extends RList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
+  override def tail: RList[Nothing] = throw new NoSuchElementException
+  override def isEmpty: Boolean = true
+
+  override def toString: String = "[]"
+
+  override def reverse: RList[Nothing] = RNil
+
+  // append another list
+  override def ++[S >: Nothing](anotherList: RList[S]): RList[S] = anotherList
+
+//  override def reverseAt: RList[Nothing] = RNil
+
+  override def removeAt(index: Int): RList[Nothing] = RNil
+}
+
+case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
+  override def isEmpty: Boolean = false
+
+  override def toString: String = {
+    @tailrec
+    def toStringTailrec(remaining: RList[T], result: String): String = {
+      if (remaining.isEmpty) result
+      else if (remaining.tail.isEmpty) s"$result${remaining.head}"
+      else toStringTailrec(remaining.tail, s"$result${remaining.head}, ")
+    }
+
+    "[" + toStringTailrec(this, "") + "]"
+  }
+
+  override def reverse: RList[T] = {
+    def reverseTailrec(remainingList: RList[T], result: RList[T]): RList[T] = {
+      if (remainingList.isEmpty) result
+      else reverseTailrec(remainingList.tail, remainingList.head :: result)
+    }
+
+    reverseTailrec(this, RNil)
+  }
+
+  // reverse this list into a new list
+  override def ++[S >: T](anotherList: RList[S]): RList[S] = {
+    def concatTailrec(remainingList: RList[S], acc:RList[S]): RList[S] = {
+      if (remainingList.isEmpty) acc
+      else concatTailrec(remainingList.tail, remainingList.head :: acc)
+    }
+    concatTailrec(anotherList, this.reverse).reverse
+  }
+
+  // remove an element
+  override def removeAt(index: Int): RList[T] = {
+    def removeAtTailrec(remaining: RList[T], currentIndex: Int, predecessors: RList[T]): RList[T] = {
+      if (currentIndex == index) predecessors.reverse ++ remaining.tail
+      else if (remaining.isEmpty) predecessors.reverse
+      else removeAtTailrec(remaining.tail, currentIndex + 1, remaining.head :: predecessors)
+    }
+    removeAtTailrec(this, 0, RNil)
+  }
+
+}
+object RList {
+  def from[T](iterable: Iterable[T]): RList[T] = {
+    def convertToRListTailrec(remaining: Iterable[T], acc: RList[T]): RList[T] = {
+      if (remaining.isEmpty) acc
+      else convertToRListTailrec(remaining.tail, remaining.head :: acc)
+    }
+
+    convertToRListTailrec(iterable, RNil).reverse
+  }
+}
+
+
+
+object RemoveKthElement extends App {
+
+
+}
