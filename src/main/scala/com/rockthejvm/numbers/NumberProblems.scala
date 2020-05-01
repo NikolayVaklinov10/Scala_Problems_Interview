b25302c2e2bf7bf6d3440481fb63e10e1f29a048
Nick <nikolayvaklinov@yahoo.com>
2020-05-01 23:56:57 +0100

    isPrime examples for numbers problems

--- src/main/scala/com/rockthejvm/numbers/NumberProblems.scala
+++ src/main/scala/com/rockthejvm/numbers/NumberProblems.scala
@@ -1,8 +1,18 @@
 package com.rockthejvm.numbers
 
+import scala.annotation.tailrec
 
 
 object NumberProblems extends App {
+
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailrec(currentDivisor: Int): Boolean = {
+      if (currentDivisor > Math.sqrt(Math.abs(n))) true
+      else n % currentDivisor != 0 && isPrimeTailrec(currentDivisor + 1)
+    }
+    isPrimeTailrec(2)
+  }
 
 }
 
