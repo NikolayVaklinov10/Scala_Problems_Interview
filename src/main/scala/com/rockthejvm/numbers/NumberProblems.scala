285f6a3afb3fdc50f528f985876ea737440982c4
Nick <nikolayvaklinov@yahoo.com>
2020-05-05 18:15:00 +0100

    decompose commit

--- src/main/scala/com/rockthejvm/numbers/NumberProblems.scala
+++ src/main/scala/com/rockthejvm/numbers/NumberProblems.scala
@@ -14,12 +14,42 @@ object NumberProblems extends App {
     isPrimeTailrec(2)
   }
 
-  println(isPrime(2))
-  println(isPrime(15))
-  println(isPrime(2003))
-  println(isPrime(23352352))
-  println(isPrime(453453453))
-  println(isPrime(1))
+  // the constituent prime divisors
+  def decompose(n: Int): List[Int] = {
+    assert(n > 0)
+
+    @tailrec
+    def decomposeTailrec(remaining: Int, currentDivisor: Int, accumulator: List[Int]): List[Int] = {
+      if (currentDivisor > Math.sqrt(n)) remaining :: accumulator
+      else if (remaining % currentDivisor == 0) decomposeTailrec(remaining / currentDivisor, currentDivisor, currentDivisor :: accumulator)
+      else decomposeTailrec(remaining, currentDivisor + 1, accumulator)
+    }
+    decomposeTailrec(n, 2, List())
+  }
+
+  def testisPrime() = {
+    println(isPrime(2))
+    println(isPrime(15))
+    println(isPrime(2003))
+    println(isPrime(23352352))
+    println(isPrime(453453453))
+    println(isPrime(1))
+  }
+
+  def testDecompose() = {
+    println(decompose(2))
+    println(decompose(15))
+    println(decompose(2003))
+    println(decompose(23352352))
+    println(decompose(453453453))
+    println(decompose(1))
+  }
+
+  testisPrime()
+  testDecompose()
+
+
+
 
 
 }
