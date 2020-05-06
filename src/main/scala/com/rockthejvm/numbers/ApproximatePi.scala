15dd7c415ee0efe3700fade419b2f7f84bd4d610
Nick <nikolayvaklinov@yahoo.com>
2020-05-06 14:00:59 +0100

    using Monte Carlo simulation to find the points outside the circle

---
+++ src/main/scala/com/rockthejvm/numbers/ApproximatePi.scala
@@ -0,0 +1,23 @@
+package com.rockthejvm.numbers
+
+import scala.util.Random
+
+object ApproximatePi extends App {
+
+  val random = new Random(System.currentTimeMillis())
+  // compute Pi using Monte Carlo
+  def approximatePi(nPoints: Int): Double = {
+
+    val nPointsInsdeCircle = (1 to nPoints).map{ _ =>
+      val x = random.nextDouble()
+      val y = random.nextDouble()
+
+      x * x + y * y
+    }.count(distance => distance < 1)
+
+    nPointsInsdeCircle * 4.0 / nPoints // approximation of PI
+  }
+  println(approximatePi(1000))
+  println(approximatePi(10000))
+
+}
