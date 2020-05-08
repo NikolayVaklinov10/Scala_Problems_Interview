4687b3d121af433e3ac39a27cc1946009e3cc679
Nick <nikolayvaklinov@yahoo.com>
2020-05-08 18:25:03 +0100

    parentheses with tailrec function

--- src/main/scala/com/rockthejvm/strings/ParenthesisProblem.scala
+++ src/main/scala/com/rockthejvm/strings/ParenthesisProblem.scala
@@ -1,6 +1,48 @@
 package com.rockthejvm.strings
 
+import scala.annotation.tailrec
+
 object ParenthesisProblem extends App {
 
+  /*
+  "()" => true
+  "()()" => true
+  "(())" => true
+  ")(" => false
+   */
+  def hasValidParentheses(string: String): Boolean = {
+    /*
+      "(())" -> vpt("(())", 0)
+      = vpt("())", 1)
+      = vpt("))", 2)
+      = vpt(")", 1)
+      = vpt("", 0)
+      = true
+      "())" -> vpt("())", 0)
+     = vpt("))", 1)
+     = vpt(")", 0)
+     = false
+     Complexity: O(n)
+     */
+
+    @tailrec
+    def validParensTailrec(remaining: String, openParens: Int): Boolean = {
+      if (remaining.isEmpty) openParens == 0
+      else if (openParens == 0 && remaining.head == ')') false
+      else if (remaining.head == '(') validParensTailrec(remaining.tail, openParens + 1)
+      else validParensTailrec(remaining.tail, openParens - 1)
+    }
+    validParensTailrec(string,0)
+  }
+
+  def testValidParentheses() = {
+    println(hasValidParentheses("()"))
+    println(hasValidParentheses(")("))
+    println(hasValidParentheses("()()"))
+    println(hasValidParentheses("(())"))
+    println(hasValidParentheses("())"))
+    println(hasValidParentheses(")()"))
+    println(hasValidParentheses("(()()(()))((((())())(())))"))
+  }
 
 }
